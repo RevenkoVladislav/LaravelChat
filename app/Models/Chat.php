97f7ba767f2818f8a1b2f957192db85ce8f54cfa
RevenkoVladislav <Vlad_reven@mail.ru>
2026-02-01 23:35:00 +0600

    table и fillable для моделей

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    protected $table = 'chats';
+
+    protected $fillable = [
+        'title',
+        'users',
+    ];
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $table = 'messages';
+    protected $fillable = [
+        'chat_id',
+        'user_id',
+        'message',
+    ];
 }
